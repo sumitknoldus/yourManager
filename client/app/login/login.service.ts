c9418d8648ae555dbe6128202e21b267cb74a9cd
sumitknoldus <sumit@knoldus.com>
2016-09-01 18:30:54 +0530

    fixed bugs

--- client/app/login/login.service.ts
+++ client/app/login/login.service.ts
@@ -1,30 +1,16 @@
 import { Injectable } from '@angular/core';
 
 import {User} from '../shared/model/user';
-import { Headers, Http, Response } from '@angular/http';
+import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
-
-var USERS =  [
-    {
-        email:'sumit@knoldus.com',
-        password:'11'
-    },
-    {
-        email:'amit@knoldus.com',
-        password:'11'
-    },
-    {
-        email:'akshayrana@knoldus.in',
-        password:'11'
-    }
-];
 
 @Injectable()
 export class LoginService {
     loginUrl = '/api/users/signin';
+    responseUser: User;
     constructor(private http: Http) {}
 
-  responseUser: User;
+
 
   login(user:User) {
     this.getLoginData(user).then(userData => this.responseUser = userData);
@@ -36,7 +22,6 @@ export class LoginService {
   }
 
   getLoginData(user: User) {
-    console.log(JSON.stringify(user))
     let headers = new Headers({
       'Content-Type': 'application/json'
     });
@@ -49,42 +34,3 @@ export class LoginService {
   }
 
 }
-
-//import {Injectable} from '@angular/core';
-//import {Router} from '@angular/router';
-//import {User} from '.././user/user'
-//
-//
-//var USERS = [
-//    new User('admin@admin.com','adm9'),
-//    new User('user1@gmail.com','a23')
-//];
-//
-//@Injectable()
-//export class LoginService {
-//
-//    constructor(
-//        private _router: Router){}
-//
-//    logout() {
-//        localStorage.removeItem("user");
-//        this._router.navigate(['Login']);
-//    }
-//
-//    login(user){
-//        var authenticatedUser = USERS.find(u => u.email === user.email);
-//        if (authenticatedUser && authenticatedUser.password === user.password){
-//            localStorage.setItem("user", authenticatedUser);
-//            this._router.navigate(['Home']);
-//            return true;
-//        }
-//        return false;
-//
-//    }
-//
-//    checkCredentials(){
-//        if (localStorage.getItem("user") === null){
-//            this._router.navigate(['Login']);
-//        }
-//    }
-//}
