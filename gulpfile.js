76ca778cbe765fdaf24fb948bbf44b1a40d188c7
sumitknoldus <sumit@knoldus.com>
2016-09-12 18:20:58 +0530

    change in config

--- gulpfile.js
+++ gulpfile.js
@@ -252,7 +252,7 @@ gulp.task('build-dev', function(callback) {
     runSequence(
         'prep-env',
         'ts-compile',
-        [ 'copy-system-lib','copy-rootfiles', 'copy-css', 'copy-corelib', 'minify-images', 'copy-html', 'copy-server', 'copy-components'],
+        [ 'copy-rootfiles', 'copy-css', 'copy-corelib', 'minify-images', 'copy-html', 'copy-server', 'copy-components'],
         ['start-server'],
         'gulp-watch',
         callback);
